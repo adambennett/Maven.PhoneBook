fac44fe517a35a8f820984e7c23ccf3ef4b84135
Nyoxide <nyoxidetwitter@gmail.com>
2020-02-21 17:09:27 -0500

    added a few lines but I'm going home

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,17 +9,25 @@ import java.util.Map;
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private Map<String, List<String>> mapp;
+
     public PhoneBook(Map<String, List<String>> map) {
+        mapp.putAll(map);
     }
 
     public PhoneBook() {
-        this(null);
+        this(new HashMap<>());
     }
 
     public void add(String name, String phoneNumber) {
+        List<String> phonesN = new ArrayList<>();
+        phonesN.add(phoneNumber);
+        mapp.put(name, phonesN);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+
     }
 
     public void remove(String name) {
