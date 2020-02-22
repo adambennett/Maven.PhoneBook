f9e8e8e0f3751efcd1c4425a0ae547437542f974
Adam Bennett <nyoxidetwitter@gmail.com>
2020-02-21 22:14:56 -0500

    finished lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,55 +1,81 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
 
-    private Map<String, List<String>> mapp;
+    private LinkedHashMap<String, List<String>> mapp;
 
     public PhoneBook(Map<String, List<String>> map) {
-        mapp.putAll(map);
+        if (!map.isEmpty()) {
+            mapp.putAll(map);
+        } else {
+            mapp = new LinkedHashMap<>();
+        }
     }
 
     public PhoneBook() {
-        this(new HashMap<>());
+        this.mapp = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
-        List<String> phonesN = new ArrayList<>();
+        ArrayList<String> phonesN = new ArrayList<>();
         phonesN.add(phoneNumber);
-        mapp.put(name, phonesN);
+        this.mapp.put(name, phonesN);
     }
 
     public void addAll(String name, String... phoneNumbers) {
-
+        ArrayList<String> toAdd = new ArrayList<>();
+        if (phoneNumbers != null) {
+            for (String s : phoneNumbers) {
+                toAdd.add(s);
+            }
+        }
+        this.mapp.put(name, toAdd);
     }
 
     public void remove(String name) {
+        this.mapp.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        if (this.mapp.containsKey(name)) {
+            return true;
+        } else {
+            for (Map.Entry<String, List<String>> i : this.mapp.entrySet()) {
+                if (i.getValue().contains(name)) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
     public List<String> lookup(String name) {
-        return null;
+       return this.mapp.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+        for (Map.Entry<String, List<String>> entry : this.mapp.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                return entry.getKey();
+            }
+        }
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> toAdd = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : this.mapp.entrySet()) {
+           toAdd.add(entry.getKey());
+        }
+        return toAdd;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.mapp;
     }
 }
